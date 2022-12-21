7ccd893fed8bd8ae454936a0eeacbdb681df1032
Монахов Максим Вадимович <monakhov@krista.ru>
2022-12-21 20:12:42 +0300

    refactor: вынес повторяющийся catch

--- lib/src/native/native.dart
+++ lib/src/native/native.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: require_trailing_commas
+
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -14,132 +16,92 @@ import 'package:path_provider/path_provider.dart';
 class Native {
   static const MethodChannel _channel = MethodChannel('crypt_signature');
 
-  static Future<bool> initCSP() async {
-    try {
-      bool? response = await _channel.invokeMethod<bool>("initCSP");
-      if (response!) return true;
-      throw ApiResponseException("Не удалось инициализировать провайдер", "Неизвестная ошибка");
-    } on ApiResponseException {
-      rethrow;
-    } on PlatformException catch (exception) {
-      throw ApiResponseException(exception.message, exception.details.toString());
-    } on Exception catch (exception) {
-      throw ApiResponseException("Ошибка при инициализации провайдера", exception.toString());
-    }
-  }
+  static Future<bool> initCSP() => _invokeWithExceptionHandler(() async {
+        bool? response = await _channel.invokeMethod<bool>("initCSP");
+        if (response!) return true;
+        throw ApiResponseException("Не удалось инициализировать провайдер", "Неизвестная ошибка");
+      }(), "Ошибка при инициализации провайдера");
 
-  static Future<Certificate> addCertificate(File file, String password) async {
-    try {
-      String? response = await _channel.invokeMethod("addCertificate", {"path": file.path, "password": password});
-      Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
-      if (map["success"] as bool) {
-        Certificate certificate = Certificate.fromBase64(map);
-        Directory directory = await getApplicationDocumentsDirectory();
-        String filePath = "${directory.path}/certificates/${certificate.uuid}.pfx";
-        File(filePath);
-        await file.copy(filePath);
-        file.delete();
-        return certificate;
-      }
-      throw ApiResponseException(map["message"] as String?, map["exception"].toString());
-    } on ApiResponseException {
-      rethrow;
-    } on PlatformException catch (exception) {
-      throw ApiResponseException(exception.message, exception.details.toString());
-    } on Exception catch (exception) {
-      throw ApiResponseException("Не удалось добавить сертификат в хранилище", exception.toString());
-    }
-  }
+  static Future<Certificate> addCertificate(File file, String password) => _invokeWithExceptionHandler(() async {
+        String? response = await _channel.invokeMethod("addCertificate", {"path": file.path, "password": password});
+        Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
+        if (map["success"] as bool) {
+          Certificate certificate = Certificate.fromBase64(map);
+          Directory directory = await getApplicationDocumentsDirectory();
+          String filePath = "${directory.path}/certificates/${certificate.uuid}.pfx";
+          File(filePath);
+          await file.copy(filePath);
+          file.delete();
+          return certificate;
+        }
+        throw ApiResponseException(map["message"] as String?, map["exception"].toString());
+      }(), "Не удалось добавить сертификат в хранилище");
 
-  static Future<License> setLicense(String licenseSerialNumber) async {
-    try {
-      String? response = await _channel.invokeMethod("setLicense", {"license": licenseSerialNumber});
-      Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
-      License license = License.fromMap(map);
-      return license;
-    } on PlatformException catch (exception) {
-      throw ApiResponseException(exception.message, exception.details.toString());
-    } on Exception catch (exception) {
-      throw ApiResponseException("Не удалось установить лицензию", exception.toString());
-    }
-  }
+  static Future<License> setLicense(String licenseSerialNumber) => _invokeWithExceptionHandler(() async {
+        String? response = await _channel.invokeMethod("setLicense", {"license": licenseSerialNumber});
+        Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
+        License license = License.fromMap(map);
+        return license;
+      }(), "Не удалось установить лицензию");
 
-  static Future<License> getLicense() async {
-    try {
-      String? response = await _channel.invokeMethod("getLicense");
-      Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
-      License license = License.fromMap(map);
-      return license;
-    } on PlatformException catch (exception) {
-      throw ApiResponseException(exception.message, exception.details.toString());
-    } on Exception catch (exception) {
-      throw ApiResponseException("Не удалось получить информацию о лицензию", exception.toString());
-    }
-  }
+  static Future<License> getLicense() => _invokeWithExceptionHandler(() async {
+        String? response = await _channel.invokeMethod("getLicense");
+        Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
+        License license = License.fromMap(map);
+        return license;
+      }(), "Не удалось получить информацию о лицензию");
 
-  static Future<DigestResult> digest(Certificate certificate, String password, String message) async {
-    try {
-      String? response = await _channel.invokeMethod("digest", {"certificateUUID": certificate.storageID, "password": password, "message": message});
-      Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
-      if (map["success"] as bool) {
-        return DigestResult(
-          certificate: certificate,
-          message: map["message"] as String,
-          digestAlgorithm: map["digestAlgorithm"] as String,
-          digest: (map["digest"] as String).replaceAll("\n", ""),
-        );
-      }
-      throw ApiResponseException(map["message"] as String?, map["exception"].toString());
-    } on PlatformException catch (exception) {
-      throw ApiResponseException(exception.message, exception.details.toString());
-    } on Exception catch (exception) {
-      throw ApiResponseException("Не удалось получить Digest", exception.toString());
-    }
-  }
+  static Future<DigestResult> digest(Certificate certificate, String password, String message) => _invokeWithExceptionHandler(() async {
+        String? response = await _channel.invokeMethod("digest", {"certificateUUID": certificate.storageID, "password": password, "message": message});
+        Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
+        if (map["success"] as bool) {
+          return DigestResult(
+            certificate: certificate,
+            message: map["message"] as String,
+            digestAlgorithm: map["digestAlgorithm"] as String,
+            digest: (map["digest"] as String).replaceAll("\n", ""),
+          );
+        }
+        throw ApiResponseException(map["message"] as String?, map["exception"].toString());
+      }(), "Не удалось получить Digest");
 
-  static Future<SignResult> sign(Certificate certificate, String password, String digest) async {
-    try {
-      String? response = await _channel.invokeMethod("sign", {"certificateUUID": certificate.storageID, "password": password, "digest": digest});
-      Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
-      if (map["success"] as bool) {
-        return SignResult(
-          certificate,
-          digest: map["digest"] as String,
-          signatureAlgorithm: map["signatureAlgorithm"] as String,
-          signature: map["signature"] as String,
-        );
-      }
-      throw ApiResponseException(map["message"] as String?, map["exception"].toString());
-    } on PlatformException catch (exception) {
-      throw ApiResponseException(exception.message, exception.details.toString());
-    } on Exception catch (exception) {
-      throw ApiResponseException("Не удалось выполнить подпись", exception.toString());
-    }
-  }
+  static Future<SignResult> sign(Certificate certificate, String password, String digest) => _invokeWithExceptionHandler(() async {
+        String? response = await _channel.invokeMethod("sign", {"certificateUUID": certificate.storageID, "password": password, "digest": digest});
+        Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
+        if (map["success"] as bool) {
+          return SignResult(
+            certificate,
+            digest: map["digest"] as String,
+            signatureAlgorithm: map["signatureAlgorithm"] as String,
+            signature: map["signature"] as String,
+          );
+        }
+        throw ApiResponseException(map["message"] as String?, map["exception"].toString());
+      }(), "Не удалось выполнить подпись");
 
-  static Future<PKCS7> createPKCS7(Certificate certificate, String password, String digest) async {
-    try {
-      String? response = await _channel.invokeMethod("createPKCS7", {"certificateUUID": certificate.storageID, "password": password, "digest": digest});
-      Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
-      if (map["success"] as bool) return PKCS7(content: map["pkcs7"] as String, signedAttributes: map["signedAttributes"] as String);
-      throw ApiResponseException(map["message"] as String?, map["exception"].toString());
-    } on PlatformException catch (exception) {
-      throw ApiResponseException(exception.message, exception.details.toString());
-    } on Exception catch (exception) {
-      throw ApiResponseException("Не удалось создать PKCS7", exception.toString());
-    }
-  }
+  static Future<PKCS7> createPKCS7(Certificate certificate, String password, String digest) => _invokeWithExceptionHandler(() async {
+        String? response = await _channel.invokeMethod("createPKCS7", {"certificateUUID": certificate.storageID, "password": password, "digest": digest});
+        Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
+        if (map["success"] as bool) return PKCS7(content: map["pkcs7"] as String, signedAttributes: map["signedAttributes"] as String);
+        throw ApiResponseException(map["message"] as String?, map["exception"].toString());
+      }(), "Не удалось создать PKCS7");
 
-  static Future<PKCS7> addSignatureToPKCS7(PKCS7 pkcs7, String signature) async {
-    try {
-      String? response = await _channel.invokeMethod("addSignatureToPKCS7", {"pkcs7": pkcs7.content, "signature": signature});
-      Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
-      if (map["success"] as bool) return PKCS7(content: map["pkcs7"] as String, signedAttributes: "");
-      throw ApiResponseException(map["message"] as String?, map["exception"].toString());
-    } on PlatformException catch (exception) {
-      throw ApiResponseException(exception.message, exception.details.toString());
-    } on Exception catch (exception) {
-      throw ApiResponseException("Не удалось добавить сигнатуру к PKCS7", exception.toString());
-    }
+  static Future<PKCS7> addSignatureToPKCS7(PKCS7 pkcs7, String signature) => _invokeWithExceptionHandler(() async {
+        String? response = await _channel.invokeMethod("addSignatureToPKCS7", {"pkcs7": pkcs7.content, "signature": signature});
+        Map<String, dynamic> map = json.decode(response!) as Map<String, dynamic>;
+        if (map["success"] as bool) return PKCS7(content: map["pkcs7"] as String, signedAttributes: "");
+        throw ApiResponseException(map["message"] as String?, map["exception"].toString());
+      }(), "Не удалось добавить сигнатуру к PKCS7");
+}
+
+Future<T> _invokeWithExceptionHandler<T>(Future<T> future, String errorMessage) async {
+  try {
+    return await future;
+  } on ApiResponseException {
+    rethrow;
+  } on PlatformException catch (exception) {
+    throw ApiResponseException(exception.message, exception.details.toString());
+  } on Exception catch (exception) {
+    throw ApiResponseException(errorMessage, exception.toString());
   }
 }
