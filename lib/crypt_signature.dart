84f54ced47aad8d28a598fe7a6d95d4d9948cf6e
Монахов Максим Вадимович <monakhov@krista.ru>
2022-12-21 18:25:45 +0300

    refactor: добавил XMLInterfaceRequest в Public API

--- lib/crypt_signature.dart
+++ lib/crypt_signature.dart
@@ -4,7 +4,7 @@ export 'src/crypt_signature_api.dart' show CryptSignature;
 export 'src/models/algorithm.dart' show Algorithm;
 export 'src/models/certificate.dart' show Certificate;
 export 'src/models/digest_result.dart' show DigestResult;
-export 'src/models/interface_request.dart' show MessageInterfaceRequest, PKCS7InterfaceRequest, PKCS7HASHInterfaceRequest;
+export 'src/models/interface_request.dart' show MessageInterfaceRequest, PKCS7MessageInterfaceRequest, PKCS7HASHInterfaceRequest, XMLInterfaceRequest;
 export 'src/models/license.dart' show License;
 export 'src/models/pkcs7.dart' show PKCS7;
 export 'src/models/sign_result.dart' show SignResult;
