fcd350bbd36297b718231a37aa55e7d43d8fe2e8
Maxim Monakhov <max.mon.vad@gmail.com>
2023-06-24 00:40:29 +0300

    test: добавил тесты для PKCS7

--- test/pkcs7_test.dart
+++ test/pkcs7_test.dart
@@ -1,14 +1,32 @@
-import 'dart:typed_data';
+import 'dart:convert';
 
+import 'package:crypt_signature/crypt_signature.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'test_data.dart';
+
 void main() {
-  group("Тестирование класса PKCS7", () {
-    List<int> list = [1, 2, 3];
-    List<Uint8List> list2 = [Uint8List.fromList(list)];
-    var result = list2.expand((x) => [1]).toList();
-    print(result);
+  group("Тестирование класса PKCS7.", () {
+    PKCS7 createPKCS7(String certificate) => PKCS7(
+          Certificate.fromBase64({"certificate": certificate, "alias": "CERT_ALIAS"}),
+          base64.encode("TEST_DIGEST".codeUnits),
+          signTime: DateTime.parse("2012-02-27 13:27:00").toUtc(),
+        );
+
+    test('В base64. Сертификат Криста УЦ', () {
+      expect(createPKCS7(TestData.kristaRawCertificate).content, TestData.kristaRawPKCS7);
+    });
+
+    test('Из base64. Сертификат КриптоПРО УЦ', () {
+      expect(createPKCS7(TestData.cryptoProRawCertificate).content, TestData.cryptoProRawPKCS7);
+    });
+
+    test('Прикрепление сигнатуры', () {
+      expect(false, TestData.cryptoProRawPKCS7 == TestData.cryptoProRawPKCS7WithSignature);
+      PKCS7 pkcs7 = createPKCS7(TestData.cryptoProRawCertificate);
+      expect(pkcs7.content, TestData.cryptoProRawPKCS7);
+      pkcs7.attachSignature(base64.encode("TEST_SIGNATURE".codeUnits));
+      expect(pkcs7.content, TestData.cryptoProRawPKCS7WithSignature);
+    });
   });
 }
-
-const int CONTEXT_SPECIFIC_TYPE = 0xA0;
