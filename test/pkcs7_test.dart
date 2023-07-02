ebb7f7d52731d96203967c557dc6158d0717f5cb
Maxim Monakhov <max.mon.vad@gmail.com>
2023-07-02 04:44:52 +0300

    test: исключение при добавлении сигнатуры к pkcs7

--- test/pkcs7_test.dart
+++ test/pkcs7_test.dart
@@ -7,10 +7,11 @@ import 'test_data.dart';
 
 void main() {
   group("Тестирование класса PKCS7.", () {
-    PKCS7 createPKCS7(String certificate) => PKCS7(
+    PKCS7 createPKCS7(String certificate, {String? signature}) => PKCS7(
           Certificate.fromBase64({"certificate": certificate, "alias": "CERT_ALIAS"}),
           base64.encode("TEST_DIGEST".codeUnits),
           signTime: DateTime.parse("2012-02-27 13:27:00").toUtc(),
+          signature: signature,
         );
 
     test('В base64. Сертификат Криста УЦ', () {
@@ -24,9 +25,23 @@ void main() {
     test('Прикрепление сигнатуры', () {
       expect(false, TestData.cryptoProRawPKCS7 == TestData.cryptoProRawPKCS7WithSignature);
       PKCS7 pkcs7 = createPKCS7(TestData.cryptoProRawCertificate);
+      int length = pkcs7.root.encodedBytes.length;
       expect(pkcs7.content, TestData.cryptoProRawPKCS7);
       pkcs7.attachSignature(base64.encode("TEST_SIGNATURE".codeUnits));
       expect(pkcs7.content, TestData.cryptoProRawPKCS7WithSignature);
+      expect(pkcs7.root.encodedBytes.length, greaterThan(length));
+    });
+
+    test('Прикрепление сигнатуры повторно', () {
+      PKCS7 pkcs7_1 = createPKCS7(TestData.kristaRawCertificate)..attachSignature(base64.encode("TEST_SIGNATURE".codeUnits));
+      PKCS7 pkcs7_2 = createPKCS7(TestData.kristaRawCertificate, signature: base64.encode("TEST_SIGNATURE".codeUnits));
+
+      expect(pkcs7_1.signature, isNotNull);
+      expect(pkcs7_2.signature, isNotNull);
+      expect(pkcs7_1.signerInfo.signature, isNotNull);
+      expect(pkcs7_2.signerInfo.signature, isNotNull);
+      expect(() => pkcs7_1.attachSignature(base64.encode("TEST_SIGNATURE".codeUnits)), throwsException);
+      expect(() => pkcs7_2.attachSignature(base64.encode("TEST_SIGNATURE".codeUnits)), throwsException);
     });
   });
 }
